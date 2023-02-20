15cc74311ae2d835ab86e082294a619820c1e201
AlannaYL <113668483+AlannaYL@users.noreply.github.com>
2023-02-20 13:29:18 +0000

    Deploying to gh-pages from @ AlannaYL/About-front@95bd180408699484af30eb7441fd8a69a351ebdb 🚀

---
+++ assets/swiper.8c70c8de.js
@@ -0,0 +1,1 @@
+import{f as e}from"./index.1e5a2f74.js";import{S as o,a as s}from"./swiper-slide.07dc155e.js";var t=e(({app:r})=>{r.use(o),r.use(s)});export{t as default};
---
+++ assets/use-quasar.857f3918.js
@@ -0,0 +1,1 @@
+import{i as a,ak as r}from"./index.1e5a2f74.js";function u(){return a(r)}export{u};
